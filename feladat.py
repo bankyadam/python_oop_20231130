5e3b0749c4a26cbddd1e171007788a4bb7ccb223
Adam Banky <banky.adam@gmail.com>
2023-11-30 17:45:25 +0100

    comment

--- feladat.py
+++ feladat.py
@@ -15,25 +15,39 @@ szalloda.uj_szoba(ketagyasszoba)
 
 foglalaskezelo = Foglalaskezelo()
 
+
+# A FOGLALASOK KEZELESE / ELOFELTOLTES
 print("A foglalas ara: ", foglalaskezelo.foglalas(szalloda, egyagyasszoba1, date(2023, 12, 28)))
 print("A foglalas ara: ", foglalaskezelo.foglalas(szalloda, ketagyasszoba, date(2023, 12, 30)))
 print("A foglalas ara: ", foglalaskezelo.foglalas(szalloda, ketagyasszoba, date(2023, 12, 6)))
+print("A foglalas ara: ", foglalaskezelo.foglalas(szalloda, egyagyasszoba1, date(2024, 1, 8)))
+print("A foglalas ara: ", foglalaskezelo.foglalas(szalloda, egyagyasszoba2, date(2024, 1, 28)))
 
+
+# A FOGLALAS KIVETELT DOB, HA MULTBELI A DATUM
 try:
     foglalaskezelo.foglalas(szalloda, ketagyasszoba, date(2020, 12, 6))
 except:
     print("❗Csak a jovobeni datumot fogadja el")
 
+
+# A FOGLALAS KIVETELT DOB, HA AZ ADOTT SZOBAT ARRA A NAPRA MAR LEFOGLALTAK
 try:
     foglalaskezelo.foglalas(szalloda, ketagyasszoba, date(2023, 12, 6))
 except:
     print("❗Ezt a szobat mar lefoglaltak erre a datumra")
 
+
+# SIKERES LEMONDAS
 foglalaskezelo.lemondas(szalloda, ketagyasszoba, date(2023, 12, 6))
 
+
+# A LEMONDAS KIVETELT DOB, HA A MEGADOTT PARAMETEREK ALAPJAN NEM TALALHATO FOGLALAS
 try:
     foglalaskezelo.lemondas(szalloda, ketagyasszoba, date(2023, 12, 6))
 except:
     print("❗Nem volt ilyen foglalas")
 
+
+# FOGLALASOK LISTAZASA
 foglalaskezelo.listazas()
